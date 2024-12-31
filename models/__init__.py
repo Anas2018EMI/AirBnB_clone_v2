86d6e23ed247e2ae60aab473a17f411ae179cd18
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-31 18:52:35 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""
+This module initializes the storage system for the HBNB project.
+"""
+from os import getenv
 
+storage_type = getenv('HBNB_TYPE_STORAGE')  # Determine the storage type from environment variables
 
-storage = FileStorage()
-storage.reload()
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()  # Initialize DBStorage if storage type is 'db'
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()  # Default to FileStorage
+
+storage.reload()  # Reload objects from storage
