5f3b95543a0fd83f900923e5a2c5b6a5bb014f91
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-01 10:19:00 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    # Define SQLAlchemy columns
+    name = Column(String(128), nullable=False)
+
+    # Define relationship with City
+    cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        """Getter attribute for FileStorage relationship.
+        Returns the list of City instances with state_id matching the current State id.
+        """
+        from models import storage
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
