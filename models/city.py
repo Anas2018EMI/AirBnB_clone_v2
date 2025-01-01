ffe8865c1120367734894fba2a8f6b441e49fb04
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-01 10:17:25 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """
+    The city class, contains state ID and name
+    """
     __tablename__ = 'cities'
 
-    # Define the name of the city as a required string column
-    name = Column(String(128), nullable=False)
-
-    # Define the state_id as a foreign key linking to the states table
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
